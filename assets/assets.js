c75324a13007ed175bb6e1eae8000ecd9430570e
Jephone Israel Jireh S. Torre <127481135+JephoneTorre@users.noreply.github.com>
2025-09-03 08:17:06 +0800

    lil changes

--- assets/assets.js
+++ assets/assets.js
@@ -122,8 +122,8 @@ export const serviceData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript, React Js, Next Js' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'UnderGraduate in Computer Science' },
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript, React Js, Next Js, Python, C++' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'UnderGraduate in Computer Science at WVSU main campus' },
     { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Collaborated and made web-app projects' }
 ];
 
